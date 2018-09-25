a7bf0b44eb6c5c781594101c836f1045a9edb4c9
ollijantunen <32096381+ollijantunen@users.noreply.github.com>
2018-09-26 00:32:24 +0300

    Initial commit.

---
+++ app/src/main/java/com/example/oj_dx13/counter/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.oj_dx13.counter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private int counterValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
